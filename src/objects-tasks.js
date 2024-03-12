e47e8bf8bd87440814253a58ffe0a1b31e5bd7ef
naijelo <naijel.a@gmail.com>
2024-03-12 14:11:39 +0300

    feat: add mergeObjects implementation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const newObj = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (Object.hasOwn(newObj, key)) {
+        newObj[key] += value;
+      } else {
+        newObj[key] = value;
+      }
+    });
+  });
+  return newObj;
 }
 
 /**
