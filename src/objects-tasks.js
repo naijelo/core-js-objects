39b87de8430fe13d06894e02c5f3ff131bebf6db
naijelo <naijel.a@gmail.com>
2024-03-12 15:23:29 +0300

    feat: add rectangle implementation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -188,8 +188,20 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  Object.defineProperty(this, 'width', {
+    get() {
+      return width;
+    },
+  });
+  Object.defineProperty(this, 'height', {
+    get() {
+      return height;
+    },
+  });
+  this.getArea = function () {
+    return height * width;
+  };
 }
 
 /**
